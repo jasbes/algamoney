301266b70724a6788293cec4e77a33a43c7d4dae
Jefferson <jefferson_uk@hotmail.com>
2019-10-30 23:18:46 +0100

    create entity entry

---
+++ algamoney-api/src/main/java/com/algaworks/algamoney/api/data/entity/EntryEntity.java
@@ -0,0 +1,52 @@
+package com.algaworks.algamoney.api.data.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "entry")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode(of = {"id"})
+public class EntryEntity {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "due_date")
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate dueDate;
+
+    @Column(name = "payment_date")
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate paymentDate;
+
+    @Column(name = "amount")
+    private BigDecimal amount;
+
+    @Column(name = "comment")
+    private LocalDate comment;
+
+    @Column(name = "type")
+    @Enumerated(EnumType.STRING)
+    private EntryType type;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private CategoryEntity category;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private ClientEntity client;
+}
---
+++ algamoney-api/src/main/java/com/algaworks/algamoney/api/data/entity/EntryType.java
@@ -0,0 +1,5 @@
+package com.algaworks.algamoney.api.data.entity;
+
+public enum EntryType {
+    IN, OUT
+}
